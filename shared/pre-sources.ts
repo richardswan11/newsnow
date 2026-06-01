cdf3d554da6710a44cfced76fc96e53afcbb0474
Richard Jia <richardjia@Richards-MacBook-Air.local>
2026-06-01 14:15:47 +0800

    remove fastbull express

--- shared/pre-sources.ts
+++ shared/pre-sources.ts
@@ -216,11 +216,6 @@ export const originSources = {
     home: "https://www.fastbull.cn",
     column: "finance",
     sub: {
-      express: {
-        title: "快讯",
-        type: "realtime",
-        interval: Time.Realtime,
-      },
       news: {
         title: "头条",
         interval: Time.Common,
